21fdae94309cf9ade003c38642216f9d7364f97a
Tupay <lutfi@mhs.ubharajaya.ac.id>
2019-10-10 11:50:13 +0700

    enhance riwayat model

--- app/Models/Anggota/RiwayatOrderDetailModel.php
+++ app/Models/Anggota/RiwayatOrderDetailModel.php
@@ -9,6 +9,8 @@ class RiwayatOrderDetailModel extends Model {
 
     protected $table = 'view_order_detail';
 
+    protected $casts = [ 'id_layanan' => 'int' ];
+
     protected $fillable = [
         'id_order',
         'id_layanan'
